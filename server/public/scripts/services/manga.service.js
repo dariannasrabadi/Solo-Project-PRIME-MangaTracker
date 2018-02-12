4a6e52796367dc4e807bf05b240083ecc400d1f0
Darian N <darian.nasrabadi@gmail.com>
2018-02-12 16:07:25 -0600

    Add favorites functionality done, alert to inform user manga was added

--- server/public/scripts/services/manga.service.js
+++ server/public/scripts/services/manga.service.js
@@ -5,7 +5,7 @@ myApp.service('MangaService', ['$http', '$location', function($http, $location){
     self.mangaResults = { list: [] }
 
     //Sending search inquiry to the service to perform get request.
-    self.searchManga = function(searchInput) {
+    self.searchManga = function(searchInput) { //Search manga function (Used in all views except register & login)
         $http.get(`/api/manga/${searchInput}`)
             .then(response => {
                 // console.log(response);
@@ -17,7 +17,19 @@ myApp.service('MangaService', ['$http', '$location', function($http, $location){
             .catch(error => {
                 console.log(error);
             })
-    };
+    }; //Search manga function (Used in all views except register & login)
+
+    self.addFavorite = function(mangaInfo) {
+        console.log('data to add to the favorites', mangaInfo);
+        $http.post(`/api/manga`, mangaInfo)
+            .then(response => {
+                console.log('added', response);   
+                alert(`${mangaInfo.title} has been added to favorites!`)             
+            })
+            .catch(error => {
+                console.log('Error on adding to favorites.', error);
+            })
+    }
 
 }]);
   
--- server/routes/manga.router.js
+++ server/routes/manga.router.js
@@ -10,7 +10,7 @@ let options_auth = { user: process.env.MAL_USER, password: process.env.MAL_PASSW
 let client = new Client(options_auth);
 
 
-router.get('/:search', (req, res) => {
+router.get('/:search', (req, res) => { // Start of search results for M.A.L. API.
     client.get(`https://myanimelist.net/api/manga/search.xml?q=${req.params.search}`, function (data, response) {
         // parsed response body as js object 
         //Adding the .manga.entry directly opens each manga details right away.
@@ -18,6 +18,22 @@ router.get('/:search', (req, res) => {
         res.send(data.manga.entry);
         // console.log('this is the raw response',response);
     });
-}); // end get
+}); // end get search results
+
+router.post('/', (req, res) => { //Start post of add new favorites.
+    // console.log('Manga Information', req.body);
+    // please see my-manga-tracker.sql for how the table is created.
+    // Checking for duplicates per user is done on the said .sql sheet. 
+    let queryText = `INSERT INTO favorites (manga_name, manga_id, user_id, manga_image_url)
+                     VALUES ('${req.body.title}', '${req.body.id}', '${req.user.id}', '${req.body.image}');`
+    pool.query(queryText)
+        .then((result) => {
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.log('Error making update query', err);
+            res.sendStatus(500);
+        });
+}); // end post of add new favorites.
 
 module.exports = router;
